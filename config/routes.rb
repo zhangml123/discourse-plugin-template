7b656bb3f91a35fc43fcd515cb971312a5b77331
zhangml123 <s15532292192@163.com>
2020-03-06 23:57:08 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ require_dependency "discourse_plugin_template_constraint"
 
 DiscoursePluginTemplate::Engine.routes.draw do
   get "/hello" => "hello#index", constraints: DiscoursePluginTemplateConstraint.new
+  get "/" => "hello#index", constraints: DiscoursePluginTemplateConstraint.new
 end
